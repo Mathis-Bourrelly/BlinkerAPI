1d7c1ce2062c61d6e51cac91851a8535e90ae469
Mathis Bourrelly <mathis.bourrelly13@gmail.com>
2025-02-28 11:09:43 +0100

    fix login

--- src/route/login.route.js
+++ src/route/login.route.js
@@ -10,10 +10,8 @@ router.get("/status", (req, res) => {
 
 router.post(
     "/login",
-    [
-        body("email").notEmpty().withMessage("L'email est requis"),
-        body("password").notEmpty().withMessage("Le mot de passe est requis"),
-    ],
+    body("email").notEmpty().withMessage("L'email est requis"),
+    body("password").notEmpty().withMessage("Le mot de passe est requis"),
     async (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -24,7 +22,8 @@ router.post(
             const result = await AuthService.login(req.body);
             res.status(200).json(result);
         } catch (error) {
-            next(error);
+            //next(error);
+            console.error(error);
         }
     }
 );
